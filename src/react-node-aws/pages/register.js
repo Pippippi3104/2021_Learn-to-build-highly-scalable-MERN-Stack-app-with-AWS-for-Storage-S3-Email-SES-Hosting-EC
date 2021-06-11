9112b19ed37f28828a2dfda5aa263422c51955af
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-11 10:02:34 +0900

    note

--- src/react-node-aws/pages/register.js
+++ src/react-node-aws/pages/register.js
@@ -1,7 +1,81 @@
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 const Register = () => {
-	return <Layout>register page</Layout>;
+	// state
+	const [state, setState] = useState({
+		name: "",
+		email: "",
+		password: "",
+		error: "",
+		success: "",
+		buttonText: "Register",
+	});
+	const { name, email, password, error, success, buttonText } = state;
+
+	// functions
+	const handleChange = (name) => (e) => {
+		setState({
+			...state,
+			[name]: e.target.value,
+			error: "",
+			success: "",
+			buttonText: "Regiser",
+		});
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.table({ name, email, password });
+	};
+
+	// components
+	const registerForm = () => (
+		<form onSubmit={handleSubmit}>
+			<div className="form-group">
+				<input
+					value={name}
+					onChange={handleChange("name")}
+					type="text"
+					className="form-control"
+					placeholder="Type your name"
+				/>
+			</div>
+			<div className="form-group">
+				<input
+					value={email}
+					onChange={handleChange("email")}
+					type="email"
+					className="form-control"
+					placeholder="Type your email"
+				/>
+			</div>
+			<div className="form-group">
+				<input
+					value={password}
+					onChange={handleChange("password")}
+					type="password"
+					className="form-control"
+					placeholder="Type your password"
+				/>
+			</div>
+			<div className="form-group">
+				<button className="btn btn-outline-warning">{buttonText}</button>
+			</div>
+		</form>
+	);
+
+	// main screen
+	return (
+		<Layout>
+			<div className="col-md-6 offset-md-3">
+				<h1>Register</h1>
+				<br />
+				{registerForm()}
+				<hr />
+				{JSON.stringify(state)}
+			</div>
+		</Layout>
+	);
 };
 
 export default Register;
