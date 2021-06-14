193b49767e0e94b1fd190af44ffec1d0513f140f
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-14 10:02:55 +0900

    note

--- src/react-node-aws/server/validators/auth.js
+++ src/react-node-aws/server/validators/auth.js
@@ -7,3 +7,10 @@ exports.userRegisterValidator = [
 		.isLength({ min: 6 })
 		.withMessage("Password must be at least 6 characters long"),
 ];
+
+exports.userLoginValidator = [
+	check("email").not().isEmpty().withMessage("Must be a email address"),
+	check("password")
+		.isLength({ min: 6 })
+		.withMessage("Password must be at least 6 characters long"),
+];
