ec526607a8b42ab5d864b905946e4deaccb5de96
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-16 22:51:16 +0900

    note

--- src/react-node-aws/server/routes/auth.js
+++ src/react-node-aws/server/routes/auth.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const {
 	userRegisterValidator,
 	userLoginValidator,
+	forgotPasswordValidator,
+	resetPasswordValidator,
 } = require("../validators/auth");
 const { runValidation } = require("../validators");
 
@@ -14,17 +16,31 @@ const {
 	registerActivate,
 	login,
 	requireSignin,
+	forgotPassword,
+	resetPassword,
 } = require("../controllers/auth");
 
 // middleware (clientからデータを受け取り、controllerへ渡す)
 router.post("/register", userRegisterValidator, runValidation, register);
 router.post("/register/activate", registerActivate);
 router.post("/login", userLoginValidator, runValidation, login);
+router.put(
+	"/forgot-password",
+	forgotPasswordValidator,
+	runValidation,
+	forgotPassword
+);
+router.put(
+	"/reset-password",
+	resetPasswordValidator,
+	runValidation,
+	resetPassword
+);
 
-router.get("/secret", requireSignin, (req, res) => {
-	res.json({
-		data: "This is secret page for logged in users only",
-	});
-});
+// router.get("/secret", requireSignin, (req, res) => {
+// 	res.json({
+// 		data: "This is secret page for logged in users only",
+// 	});
+// });
 
 module.exports = router;
