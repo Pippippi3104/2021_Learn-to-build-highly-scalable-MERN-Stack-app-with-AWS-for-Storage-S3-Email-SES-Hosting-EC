b77df83f0dfa36c7d2fd7b64613867747dbf12fa
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-19 12:26:41 +0900

    note

--- src/react-node-aws/server/validators/caterory.js
+++ src/react-node-aws/server/validators/caterory.js
@@ -2,8 +2,11 @@ const { check } = require("express-validator");
 
 exports.categoryCreateValidator = [
 	check("name").not().isEmpty().withMessage("Name is required"),
-	check("image").not().isEmpty().withMessage("Image is required"),
-	check("content").isLength({ min: 20 }).withMessage("Content is required"),
+	check("image").isEmpty().withMessage("Image is required"),
+	check("content")
+		.not()
+		.isLength({ min: 20 })
+		.withMessage("Content is required"),
 ];
 
 exports.categoryUpdateValidator = [
