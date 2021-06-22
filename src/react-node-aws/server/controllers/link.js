62c964f70ea5364e2d5a2f6cfad29068999b182b
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-23 08:18:25 +0900

    note

--- src/react-node-aws/server/controllers/link.js
+++ src/react-node-aws/server/controllers/link.js
@@ -3,11 +3,35 @@ const slugify = require("slugify");
 
 // CRUD
 exports.create = (req, res) => {
+	// state
 	const { title, url, categories, type, medium } = req.body;
-	console.table({ title, url, categories, type, medium });
+	const slug = url;
+
+	// link
+	let link = new Link({ title, url, categories, type, medium, slug });
+	link.postedBy = req.user._id; // posted by user
+	let arrayofCategories = categories && categories.split(",");
+	link.categories = arrayofCategories;
+
+	// save link
+	link.save((err, data) => {
+		if (err) {
+			return res.status(400).json({
+				error: "Link already exist",
+			});
+		}
+		res.json(data);
+	});
 };
 exports.list = (req, res) => {
-	//
+	Link.find({}).exec((err, data) => {
+		if (err) {
+			return res.status(400).json({
+				error: "Could not list links",
+			});
+		}
+		res.json(data);
+	});
 };
 exports.read = (req, res) => {
 	//
