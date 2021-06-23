085efabcdca66004766c3b09ae6b871132999582
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-23 22:06:16 +0900

    note

--- src/react-node-aws/server/routes/link.js
+++ src/react-node-aws/server/routes/link.js
@@ -12,7 +12,14 @@ const { runValidation } = require("../validators");
 
 // import from controllers
 const { requireSignin, authMiddleware } = require("../controllers/auth");
-const { create, list, read, update, remove } = require("../controllers/link");
+const {
+	create,
+	list,
+	read,
+	update,
+	remove,
+	clickCount,
+} = require("../controllers/link");
 
 // routes
 router.post(
@@ -24,6 +31,7 @@ router.post(
 	create
 );
 router.get("/links", list);
+router.put("/click-count", clickCount);
 router.get("/link/:slug", read);
 router.put(
 	"/link/:slug",
--- src/react-node-aws/server/server.js
+++ src/react-node-aws/server/server.js
@@ -13,6 +13,7 @@ mongoose
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true,
+		useFindAndModify: false,
 	})
 	.then(() => console.log("DB connected"))
 	.catch((err) => console.log(err));
