436a11849c1fb1ae5f43e6d0b6bd6bba2d72187d
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-06-24 22:23:07 +0900

    note

--- src/react-node-aws/client/pages/admin/index.js
+++ src/react-node-aws/client/pages/admin/index.js
@@ -20,6 +20,11 @@ const Admin = ({ user }) => (
 							<a className="nav-link">Categories</a>
 						</Link>
 					</li>
+					<li className="nav-item">
+						<Link href="/admin/link/read">
+							<a className="nav-link">All links</a>
+						</Link>
+					</li>
 				</ul>
 			</div>
 			<div className="col-md-8"></div>
